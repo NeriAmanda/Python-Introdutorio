2ac5c3a5972908ecbf8d95e4501321ce3c10dc00
Amanda Neri <168038088+NeriAmanda@users.noreply.github.com>
2025-06-19 19:02:42 -0300

    Add files via upload

---
+++ Fundamentos/variaveis_exercicios.py
@@ -0,0 +1,26 @@
+# Exercícios de fixação
+# Tente criar as soluções para os seguintes desafios:
+
+# Exercício 1:
+# Crie uma variável chamada 'cidade' e atribua o nome da sua cidade favorita.
+# Depois, imprima: "Minha cidade favorita é <cidade>."
+
+# Código:
+
+cidade = "Sua cidade aqui"
+print(f"Minha cidade favorita é {cidade}.")
+
+# Exercício 2:
+# Crie duas variáveis inteiras 'num1' e 'num2'.
+# Some os valores e imprima o resultado.
+
+# Código:
+num1 = 10
+num2 = 20
+soma = num1 + num2
+print(f"A soma de {num1} + {num2} é {soma}.")
+
+
+
+
+
